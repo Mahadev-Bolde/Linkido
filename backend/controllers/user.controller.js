c848daf89554abf20745844cd4b457fc16b6eda7
Mahadev Bolde <mahadevbolde36@gmail.com>
2026-03-14 19:51:39 +0530

    added the functinality of updateUserProfile and getUserandProfile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -69,3 +69,47 @@ export const uploadProfilePicture = async (req, res) => {
     return res.status(500).json({ message: err.message });
   }
 };
+
+export const updateUserProfile = async (req, res) => {
+  try {
+    const [token, ...newUserData] = req.body;
+
+    const user = await User.findOne({ token: token });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const { username, email } = newUserData;
+
+    const existingUser = await User.findOne({
+      $or: [{ username: username }, { email: email }],
+    });
+
+    if (existingUser) {
+      if (existingUser || String(existingUser._id) !== String(!user._id)) {
+        return res.status(400).json({ message: "User already exists" });
+      }
+    }
+
+    Object.assign(user, newUserData);
+
+    await User.save();
+    res.json({ messsage: "User updated" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+export const getUserandProfile = async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = await User.findOne({ token: token });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const userProfile = await profileModel
+      .findOne({ userId: user._id })
+      .populate("userId", "name email username profilePicture");
+
+    return res.json(userProfile);
+  } catch (err) {
+    res.status(500).json({ message: err.messasge });
+  }
+};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,6 +4,8 @@ import {
   register,
   login,
   uploadProfilePicture,
+  updateUserProfile,
+  getUserandProfile,
 } from "../controllers/user.controller.js";
 import multer from "multer";
 
@@ -23,5 +25,7 @@ router
   .post(upload.single("profile_picture"), uploadProfilePicture);
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/user_update").post(updateUserProfile);
+router.route("/get_user_and_profile").get(getUserandProfile);
 
 export default router;
